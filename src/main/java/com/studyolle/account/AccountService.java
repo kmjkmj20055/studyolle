0c1c4329f2378bd8914c38ad656d22740a0c162f
김민정 <>
2020-07-19 23:14:34 +0900

    회원 가입 완료 후 자동 로그인
    - 회원 가입 완료시 자동 로그인
    - 이메일 인증 완료시 자동 로그인
    
    - 스프링 시큐리티 관점에서 로그인
      - SecurityContext에 Authentication(Token)이 존재하는가?
      - UsernamePasswordAuthenticationToken
    
    - AccountControllerTest 오류하는중
      - 인증 메일 확인 - 입력값 정상 -> 여기

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -2,13 +2,20 @@ package com.studyolle.account;
 
 import com.studyolle.Account;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +26,12 @@ public class AccountService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void processNewAccount(SignUpForm signUpForm) {
+    public Account processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccount(signUpForm);
         newAccount.generateEmailCheckToken();  // 랜덤으로 token 값 생성
         sendSignUpConfirmEmail(newAccount);
+
+        return newAccount;
     }
 
     private Account saveNewAccount(@Valid SignUpForm signUpForm) {
@@ -44,5 +53,13 @@ public class AccountService {
         mailMessage.setSubject("스터디올래, 회원 가입 인증");  // 메일 제목
         mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());  // 메일 본문
         javaMailSender.send(mailMessage);
+    }
+
+    public void login(Account account) {
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(account.getNickname(), account.getPassword());
+        List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        SecurityContextHolder.getContext().setAuthentication(token);
+
     }
 }
--- src/test/java/com/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -18,6 +18,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -41,7 +43,8 @@ class AccountControllerTest {
     void signUpForm() throws Exception {
         mockMvc.perform(get("/sign-up")).andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
-                .andExpect(model().attributeExists("signUpForm"));
+                .andExpect(model().attributeExists("signUpForm"))
+                .andExpect(unauthenticated());
     }
 
     @DisplayName("회원 가입 처리 - 입력값 오류")
@@ -53,7 +56,8 @@ class AccountControllerTest {
                 .param("password","12345")
                 .with(csrf()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(unauthenticated());
     }
 
     @DisplayName("회원 가입 처리 - 입력값 정상")
@@ -65,7 +69,8 @@ class AccountControllerTest {
                 .param("password","12345678")
                 .with(csrf()))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/"));
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated().withUsername("mimmi"));
 
         Account account = accountRepository.findByEmail("kmjkmj20055@gmail.com");
         assertNotNull(account);
@@ -83,7 +88,8 @@ class AccountControllerTest {
                 .param("email","email@email.com"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("error"))
-                .andExpect(view().name("account/checked-email"));
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(unauthenticated());
     }
 
     @DisplayName("인증 메일 확인 - 입력값 정상")
@@ -92,13 +98,11 @@ class AccountControllerTest {
         Account account = Account.builder()
                                 .email("test@email.com")
                                 .password("12345678")
-                                .nickname("test")
+                                .nickname("mimmi")
                                 .build();
 
         Account newAccount = accountRepository.save(account);
         newAccount.generateEmailCheckToken();
-
-
 
         mockMvc.perform(get("/check-email-token")
                 .param("token",newAccount.getEmailCheckToken())
@@ -107,6 +111,7 @@ class AccountControllerTest {
                 .andExpect(model().attributeDoesNotExist("error"))
                 .andExpect(model().attributeExists("nickname"))
                 .andExpect(model().attributeExists("numberOfUser"))
-                .andExpect(view().name("account/checked-email"));
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(authenticated());
     }
 }
